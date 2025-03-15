b95f073ed3f7b4ebad833873db62273dc7ab3c39
rlarldud1234 <rlarldud6482@naver.com>
2025-03-15 22:41:19 +0900

    new : styles fonts

---
+++ src/styles/font.ts
@@ -0,0 +1,201 @@
+export const fonts = {
+  heading: {
+    xxl: {
+      bold: {
+        fontWeight: 700,
+        fontSize: "40px",
+        lineHeight: "1.3em",
+      },
+      semiBold: {
+        fontWeight: 600,
+        fontSize: "40px",
+        lineHeight: "1.3em",
+      },
+      medium: {
+        fontWeight: 500,
+        fontSize: "40px",
+        lineHeight: "1.3em",
+      },
+    },
+    xl: {
+      bold: {
+        fontWeight: 700,
+        fontSize: "36px",
+        lineHeight: "1.3em",
+      },
+      semiBold: {
+        fontWeight: 600,
+        fontSize: "36px",
+        lineHeight: "1.3em",
+      },
+      medium: {
+        fontWeight: 500,
+        fontSize: "36px",
+        lineHeight: "1.3em",
+      },
+    },
+    lg: {
+      bold: {
+        fontWeight: 700,
+        fontSize: "32px",
+        lineHeight: "1.4em",
+      },
+      semiBold: {
+        fontWeight: 600,
+        fontSize: "32px",
+        lineHeight: "1.4em",
+      },
+      medium: {
+        fontWeight: 500,
+        fontSize: "32px",
+        lineHeight: "1.4em",
+      },
+    },
+    md: {
+      bold: {
+        fontWeight: 700,
+        fontSize: "28px",
+        lineHeight: "1.4em",
+      },
+      semiBold: {
+        fontWeight: 600,
+        fontSize: "28px",
+        lineHeight: "1.4em",
+      },
+      medium: {
+        fontWeight: 500,
+        fontSize: "28px",
+        lineHeight: "1.4em",
+      },
+    },
+    sm: {
+      bold: {
+        fontWeight: 700,
+        fontSize: "24px",
+        lineHeight: "1.4em",
+      },
+      semiBold: {
+        fontWeight: 600,
+        fontSize: "24px",
+        lineHeight: "1.4em",
+      },
+      medium: {
+        fontWeight: 500,
+        fontSize: "24px",
+        lineHeight: "1.4em",
+      },
+    },
+  },
+  body: {
+    xl: {
+      bold: {
+        fontWeight: 700,
+        fontSize: "20px",
+        lineHeight: "1.5em",
+      },
+      semiBold: {
+        fontWeight: 600,
+        fontSize: "20px",
+        lineHeight: "1.5em",
+      },
+      medium: {
+        fontWeight: 500,
+        fontSize: "20px",
+        lineHeight: "1.5em",
+      },
+      regular: {
+        fontWeight: 400,
+        fontSize: "20px",
+        lineHeight: "1.5em",
+      },
+    },
+    lg: {
+      bold: {
+        fontWeight: 700,
+        fontSize: "18px",
+        lineHeight: "1.5em",
+      },
+      semiBold: {
+        fontWeight: 600,
+        fontSize: "18px",
+        lineHeight: "1.5em",
+      },
+      medium: {
+        fontWeight: 500,
+        fontSize: "18px",
+        lineHeight: "1.5em",
+      },
+      regular: {
+        fontWeight: 400,
+        fontSize: "18px",
+        lineHeight: "1.5em",
+      },
+    },
+    md: {
+      bold: {
+        fontWeight: 700,
+        fontSize: "16px",
+        lineHeight: "1.5em",
+      },
+      semiBold: {
+        fontWeight: 600,
+        fontSize: "16px",
+        lineHeight: "1.5em",
+      },
+      medium: {
+        fontWeight: 500,
+        fontSize: "16px",
+        lineHeight: "1.5em",
+      },
+      regular: {
+        fontWeight: 400,
+        fontSize: "16px",
+        lineHeight: "1.5em",
+      },
+    },
+    sm: {
+      bold: {
+        fontWeight: 700,
+        fontSize: "15px",
+        lineHeight: "1.5em",
+      },
+      semiBold: {
+        fontWeight: 600,
+        fontSize: "15px",
+        lineHeight: "1.5em",
+      },
+      medium: {
+        fontWeight: 500,
+        fontSize: "15px",
+        lineHeight: "1.5em",
+      },
+      regular: {
+        fontWeight: 400,
+        fontSize: "15px",
+        lineHeight: "1.5em",
+      },
+    },
+    xsm: {
+      bold: {
+        fontWeight: 700,
+        fontSize: "14px",
+        lineHeight: "1.5em",
+      },
+      semiBold: {
+        fontWeight: 600,
+        fontSize: "14px",
+        lineHeight: "1.5em",
+      },
+      medium: {
+        fontWeight: 500,
+        fontSize: "14px",
+        lineHeight: "1.5em",
+      },
+      regular: {
+        fontWeight: 400,
+        fontSize: "14px",
+        lineHeight: "1.5em",
+      },
+    },
+  },
+};
