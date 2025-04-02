972b3578fef9f2f57cf11f328deae2eec902a74a
rlarldud1234 <rlarldud6482@naver.com>
2025-04-02 15:43:41 +0900

    new : set Component

--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,29 +1,43 @@
-import { BannerImage } from "@/assets/images";
-import ChapterSection from "@/components/main/ChapterSection";
+import { Banner2Image, BannerImage } from "@/assets/images";
+import ChapterFirstSection from "@/components/main/ChapterFirstSection";
+import ChapterFourthSection from "@/components/main/ChapterFourthSection";
+import ChapterSecondSection from "@/components/main/ChapterSecondSection";
+import ChapterThirdSection from "@/components/main/ChapterThirdSection";
+import ThirdBanner from "@/components/main/ThirdBanner";
 import { color } from "@/styles/color";
 import styled from "@emotion/styled";
 import Image from "next/image";
 
 const MainPage = () => {
   return (
-    <Container>
-      <Padding />
-      <Banner src={BannerImage} alt="Banner" />
-      <Padding />
-      <ChapterSection />
-    </Container>
+    <>
+      <Container>
+        <Banner src={BannerImage} alt="Banner" />
+        <Padding />
+        <ChapterFirstSection />
+        <ChapterSecondSection />
+        <SecondBanner src={Banner2Image} alt="Banner2" />
+        <ChapterThirdSection />
+        <ChapterFourthSection />
+        <ThirdBanner />
+      </Container>
+    </>
   );
 };
 
 const Container = styled.div`
   background-color: ${color.gray[50]};
-  padding: 0 15rem;
+  padding: 32px 15rem;
   margin-top: 84px;
 `;
 const Padding = styled.div`
   height: 32px;
 `;
 const Banner = styled(Image)`
+  width: 1440px;
+  height: auto;
+`;
+const SecondBanner = styled(Image)`
   width: 1440px;
   height: auto;
 `;
